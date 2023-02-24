b124bba6207b101211449d4e77b73a7f48aa8863
Ogaranya <enomahog@gmail.com>
2023-02-24 18:23:13 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useRef } from "react";
+
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box() {
+  const ref = useRef()
+  useFrame(state => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+  });
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <Canvas style={{background: "black"}}>
-        <mesh>
-          <boxBufferGeometry />
-          <meshBasicMaterial color="blue"/>
-        </mesh>
+      <Canvas style={{ background: "black" }}>
+        <Box />
+        <axesHelper/> 
       </Canvas>
     </div>
   );
