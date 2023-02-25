49952382e2b8ee1436724cd787aaf22285e91ebf
Ogaranya <enomahog@gmail.com>
2023-02-25 14:53:23 +0100

    app

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function Orit() {
 function Box(props) {
   const ref = useRef();
   useFrame((state) => {
-    ref.current.rotation.x += 0.01;
-    ref.current.rotation.y += 0.01;
+    // ref.current.rotation.x += 0.01;
+    ref.current.rotation.y -= 0.01;
   });
   return (
     <mesh ref={ref} {...props} castShadow receiveShadow>
@@ -28,7 +28,7 @@ function Box(props) {
 
 function Floor(props) {
   return (
-    <mesh {...props} receiveShadow>
+    <mesh {...props} castShadow receiveShadow>
       <boxGeometry args={[20, 1, 10]} />
       <meshPhysicalMaterial />
     </mesh>
@@ -39,24 +39,25 @@ function Bulb(props) {
   return (
     <mesh {...props}>
       <pointLight castShadow />
-      <sphereGeometry args={[0.2]} />
+      <sphereGeometry args={[0.2, 20, 20]} />
       <meshPhongMaterial emissive="yellow" />
     </mesh>
-  ); 
+  );
 }
 
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <Canvas
-        shadowMap
+        shadows={{ type: "BasicShadowMap" }}
         style={{ background: "black" }}
-        camera={{ position: [3, 3, 3] }}
+        camera={{ position: [1, 5, 1] }}
       >
+        <fog attach="fog" args={["white", 1, 10]}/>
         <ambientLight intensity={0.2} />
         <Bulb position={[0, 3, 0]} />
         <pointLight />
-        <Box position={[-1, 1, 2]} />
+        <Box position={[0, 1, 0]} />
         <Orit />
         <axesHelper args={[5]} />
         <Floor position={[0, -0.5, 0]} />
