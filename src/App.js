83b58470a7a561cb6e1fa39b5e2869d4d618557b
Ogaranya <enomahog@gmail.com>
2023-02-25 14:21:20 +0100

    app

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import { useRef } from "react";
 
 import "./App.css";
 // import THREE from 'three/src/Three';
-import { Canvas, useFrame, useThree,  extend } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // const Geometry = new THREE.Geometry()
-extend({ OrbitControls })
+extend({ OrbitControls });
 
 function Orit() {
-  const {camera, gl} = useThree()
-  return(
-    <orbitControls args={[camera, gl.domElement]}/>
-  )
+  const { camera, gl } = useThree();
+  return <orbitControls args={[camera, gl.domElement]} />;
 }
 
 function Box(props) {
-  const ref = useRef()
-  useFrame(state => {
-    ref.current.rotation.x += 0.01
-    ref.current.rotation.y += 0.01
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
   });
   return (
-    <mesh ref={ref} {...props}>
-      <boxBufferGeometry />
+    <mesh ref={ref} {...props} castShadow receiveShadow>
+      <boxGeometry />
       <meshPhysicalMaterial color="blue" />
     </mesh>
   );
@@ -30,8 +28,8 @@ function Box(props) {
 
 function Floor(props) {
   return (
-    <mesh {...props}>
-      <boxBufferGeometry args={[20, 1, 10]} />
+    <mesh {...props} receiveShadow>
+      <boxGeometry args={[20, 1, 10]} />
       <meshPhysicalMaterial />
     </mesh>
   );
@@ -40,23 +38,28 @@ function Floor(props) {
 function Bulb(props) {
   return (
     <mesh {...props}>
-      <sphereBufferGeometry/>
-      <meshPhongMaterial emissive="yellow"/>
+      <pointLight castShadow />
+      <sphereGeometry args={[0.2]} />
+      <meshPhongMaterial emissive="yellow" />
     </mesh>
-  );
+  ); 
 }
 
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <Canvas style={{ background: "black" }} camera={{position: [3,3,3]}}>
-        <ambientLight intensity={.2}/>
-        <pointLight/> 
-        <Bulb/>
-        <Box position={[-1,1,2]}/>
-        <Orit/>
-        <axesHelper args={[5]}/> 
-        <Floor position={[0, -0.5, 0]}/>
+      <Canvas
+        shadowMap
+        style={{ background: "black" }}
+        camera={{ position: [3, 3, 3] }}
+      >
+        <ambientLight intensity={0.2} />
+        <Bulb position={[0, 3, 0]} />
+        <pointLight />
+        <Box position={[-1, 1, 2]} />
+        <Orit />
+        <axesHelper args={[5]} />
+        <Floor position={[0, -0.5, 0]} />
       </Canvas>
     </div>
   );
